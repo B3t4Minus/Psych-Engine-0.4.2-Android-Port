5c0df4a0e41a0cc65d59e7b3426b73b80ff43e2e
B3t4Minus <96133405+B3t4Minus@users.noreply.github.com>
2021-12-16 08:51:08 -0600

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,8 +46,12 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
+            ['ummmm'],
+            ['B3t4 Minus',		    'betaminus',		    'Just Porting The Mod XD',	 'https://youtube.com/channel/UCfpSyqZSPOyVfaoHqGXtJzw',	'0xFFFF33'],
+            ['']
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['']
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
